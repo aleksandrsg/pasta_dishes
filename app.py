cbe11866a28f217efa27b6d40da6ac66bc4a674c
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-04-19 16:18:53 +0000

    Connection with MongoDB

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'pasta_dishes'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tuser@myfirstcluster-1aric.mongodb.net/pasta_dishespython3?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Pasta dishes'
+@app.route('/get_receipts')
+def get_receipts():
+    return render_template("receipts.html", receipts=mongo.db.receipts.find())
 
 
 if __name__ == '__main__':
