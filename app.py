5c586c54e0c1fc3f756aad322e92eea387572db9
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-04-22 22:02:13 +0000

    Added route for insert receipt to mongoDB

--- app.py
+++ app.py
@@ -11,14 +11,25 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+
+
 @app.route('/get_receipts')
 def get_receipts():
     return render_template("receipts.html", receipts=mongo.db.receipts.find())
+
 
 @app.route('/add_receipt')
 def add_receipt():
     return render_template("addreceipt.html", receipts=mongo.db.receipts.find())
 
+
+@app.route('/insert_receipt', methods=['POST'])
+def insert_receipt():
+    receipts = mongo.db.tasks
+    receipts.insert_one(request.form.to_dict())
+    return redirect (url_for('get_receipts'))
+
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
