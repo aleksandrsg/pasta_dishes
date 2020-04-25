227e1b012043b060ea220b188b0e1c31e7a4db8c
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-04-25 19:07:10 +0000

    Added Edit Receipt Template

--- app.py
+++ app.py
@@ -29,6 +29,24 @@ def insert_receipt():
     receipts.insert_one(request.form.to_dict())
     return redirect (url_for('get_receipts'))
 
+@app.route('/edit_receipt/<receipt_id>')
+def edit_receipt(receipt_id):
+    the_receipt =  mongo.db.receipts.find_one({"_id": ObjectId(receipt_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editreceipt.html', receipt=the_receipt)
+
+@app.route('/update_receipt/<receipt_id>', methods=["POST"])
+def update_receipt(receipt_id):
+    receipts = mongo.db.receipts
+    receipts.update( {'_id': ObjectId(receipt_id)},
+    {
+        'receipt_name':request.form.get('receipt_name'),
+        'serves':request.form.get('serves'),
+        'prep': request.form.get('prep'),
+        'cook_time': request.form.get('cook_time'),
+        'ingredients':request.form.get('ingredients')
+    })
+    return redirect(url_for('get_tasks'))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
